2e9c5a1065b5ed2bd341c6cb357f2d70b3f8af3b
ccmarin14 <cristiancamilomarinflorez@gmail.com>
2024-05-17 19:45:41 -0500

    CI: Add model productos

---
+++ src/models/Productos.ts
@@ -0,0 +1,73 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../sequelize';
+
+interface ProductoAttributes {
+  handle: string;
+  title: string;
+  description: string;
+  sku: string;
+  grams: number;
+  stock: number;
+  price: number;
+  compare_price: number;
+  barcode: string | null;
+}
+
+class Producto extends Model<ProductoAttributes> implements ProductoAttributes {
+  public handle!: string;
+  public title!: string;
+  public description!: string;
+  public sku!: string;
+  public grams!: number;
+  public stock!: number;
+  public price!: number;
+  public compare_price!: number;
+  public barcode!: string | null;
+}
+
+Producto.init({
+  handle: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  sku: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: ''
+  },
+  grams: {
+    type: DataTypes.FLOAT,
+    allowNull: false
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  price: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  compare_price: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  barcode: {
+    type: DataTypes.STRING,
+    allowNull: true
+  }
+}, {
+  sequelize,
+  modelName: 'Producto',
+  tableName: 'productos'
+});
+
+export default Producto;
